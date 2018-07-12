2042b42c92435a5f2545950a8fd1d1569c12e0e1
SnowLord <31230715+SnowLord7@users.noreply.github.com>
2018-07-12 10:59:30 -0700

    Settings
    Press 'ESC' to toggle menu
    Added FPS counter

--- bundle.js
+++ bundle.js
@@ -1,6 +1,29 @@
+console.log("script.js has loaded");
+
 var init = true,
 playing = false,
+paused = false,
 fps = 0,
+frames = [],
 difficulty;
 
-console.log("script.js has loaded")
+(function refreshLoop() {
+	window.requestAnimationFrame(function() {
+		const now = performance.now();
+		while (frames.length > 0 && frames[0] <= now - 1000) {
+			frames.shift();
+		}
+		frames.push(now);
+		fps = frames.length;
+		if (playing) {
+			var canvas = document.getElementById("canvas")
+			var ctx = canvas.getContext("2d")
+			canvas.width = innerWidth;
+			canvas.height = innerHeight;
+			ctx.font = "15px Arial";
+			ctx.fillStyle = "white";
+			ctx.fillText("FPS: " + fps, 10, 20);
+		}
+		refreshLoop();
+	});
+})();
