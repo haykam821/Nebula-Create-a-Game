1399d1c221988ad15ed268eea18250f1140dea6b
SnowLord <31230715+SnowLord7@users.noreply.github.com>
2018-07-13 11:47:18 -0700

    Moved bundle.js to index.js
    Whoops

--- index.js
+++ index.js
@@ -1,79 +1,91 @@
 console.log("script.js has loaded");
 
-let init = true,
-	playing = false,
-	paused = false,
-	fps = 0,
-	frames = [],
-	difficulty;
+var initiated = true,
+playing = false,
+paused = false,
+challengeLevel = 1,
+fps = 0,
+frames = [],
+difficulty;
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+(function refreshLoop() {
+	window.requestAnimationFrame(function() {
+		const now = performance.now();
+		while (frames.length > 0 && frames[0] <= now - 1000) {
+			frames.shift();
+		}
+		frames.push(now);
+		fps = frames.length;
+		if (playing) {
+			var canvas = document.getElementById("canvas")
+			var ctx = canvas.getContext("2d")
+			canvas.width = innerWidth;
+			canvas.height = innerHeight;
+			ctx.font = "15px Arial";
+			ctx.fillStyle = "white";
+			ctx.fillText("FPS: " + fps, 10, 20);
+		}
+		refreshLoop();
+	});
+})();
 
-function refreshLoop() {
-	const now = performance.now();
-	while (frames.length > 0 && frames[0] <= now - 1000) {
-		frames.shift();
-	}
-	frames.push(now);
-	fps = frames.length;
-	if (playing) {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-		ctx.font = "15px Ubuntu";
-		ctx.fillStyle = "white";
-		ctx.fillText("FPS: " + fps, 10, 20);
-	}
+function init() {
+	var buttons = document.getElementsByClassName("growButton");
+	var boxes = document.getElementsByClassName("boxes");
 
-	window.requestAnimationFrame(refreshLoop);
-}
-refreshLoop();
-
-function initFunc() {
-	const buttons = document.getElementsByClassName("growButton");
-	for (let i = 0; i < buttons.length; i++) {
+	// Setup Expandable Buttons
+	for (var i = 0; i < buttons.length; i++) {
 		buttons[i].addEventListener("click", function() {
 			this.classList.toggle("active");
-			const panel = this.nextElementSibling;
-			if (panel.style.maxHeight) {
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight){
 				panel.style.maxHeight = null;
 			} else {
 				panel.style.maxHeight = panel.scrollHeight + "px";
-			}
+			} 
 		});
 	}
+	// Setup Table Buttons / Boxes
+	for (var i = 0; i < boxes.length; i++) {
+		for (var j = 0; j < boxes[i].children.length; j++) {
+			boxes[i].children[j].addEventListener("click", function() {
+				startChallenge(this.innerText);
+	        });
+	    }
+	}
+	// While playing if escape is pressed, pause!
+	document.addEventListener("keydown", function(e) {
+		if (e.keyCode == 27 && playing) {
+			pauseGame();
+		}
+	});
 }
-window.addEventListener("load", initFunc);
 
 function startGame() {
-	const menu = document.getElementById("menuWrapper");
-	const canvas = document.getElementById("canvas");
+	var menu = document.getElementById("menuWrapper");
+	var canvas = document.getElementById("canvas");
 	menu.classList.toggle("menuShrinkOut");
 	setTimeout(function() {
 		canvas.style.display = "block";
 		playing = true;
 	}, 450);
 }
-document.getElementById("endlessButton").addEventListener("click", startGame);
 
 function endGame() {
-	const menu = document.getElementById("menuWrapper");
-	const canvas = document.getElementById("canvas");
+	var menu = document.getElementById("menuWrapper");
+	var canvas = document.getElementById("canvas");
 	canvas.style.display = "block";
 	playing = false;
 	menu.classList.toggle("menuShrinkOut");
 }
 
 function pauseGame() {
-	const menu = document.getElementById("menuWrapper");
-	const canvas = document.getElementById("canvas");
+	var menu = document.getElementById("menuWrapper");
+	var canvas = document.getElementById("canvas");
 	paused = !paused;
-	menu.classList.toggle("menuShrinkOut");
+	menu.classList.toggle("menuShrinkOut");	
 }
 
-document.addEventListener("keydown", function(e) {
-	e.preventDefault();
-	if (e.keyCode == 27 && playing) {
-		pauseGame();
-	}
-});
+function startChallenge(level) {
+	challengeLevel = Number(level);
+}
