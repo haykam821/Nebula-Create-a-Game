3eee731d7834073bae66c0630e1eb1d42c89f92e
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-14 06:58:14 -0400

    Mild jQuery support
    This is unfinished and the collapsing menus have broke because of it (well, because someone used `this` too much).

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("script.js has loaded");
+const $ = require("jquery-browserify");
 
 let init = true,
 	playing = false,
@@ -7,8 +7,8 @@ let init = true,
 	frames = [],
 	difficulty;
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = $("#canvas");
+const ctx = canvas.get(0).getContext("2d");
 
 function refreshLoop() {
 	const now = performance.now();
@@ -18,8 +18,8 @@ function refreshLoop() {
 	frames.push(now);
 	fps = frames.length;
 	if (playing) {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
+		canvas.attr("width", window.innerWidth);
+		canvas.attr("height", window.innerHeight);
 		ctx.font = "15px Ubuntu";
 		ctx.fillStyle = "white";
 		ctx.fillText("FPS: " + fps, 10, 20);
@@ -30,50 +30,46 @@ function refreshLoop() {
 refreshLoop();
 
 function initFunc() {
-	const buttons = document.getElementsByClassName("growButton");
-	for (let i = 0; i < buttons.length; i++) {
-		buttons[i].addEventListener("click", function() {
-			this.classList.toggle("active");
-			const panel = this.nextElementSibling;
+	const buttons = $(".growButton");
+	buttons.each(function() {
+		$(this).on("click", () => {
+			$(this).classList.toggle("active");
+			const panel = $(this).nextElementSibling;
 			if (panel.style.maxHeight) {
 				panel.style.maxHeight = null;
 			} else {
 				panel.style.maxHeight = panel.scrollHeight + "px";
 			}
 		});
-	}
+	});
 }
-window.addEventListener("load", initFunc);
+$(window).on("load", initFunc);
+
+const menu = $("#menuWrapper");
 
 function startGame() {
-	const menu = document.getElementById("menuWrapper");
-	const canvas = document.getElementById("canvas");
 	menu.classList.toggle("menuShrinkOut");
 	setTimeout(function() {
 		canvas.style.display = "block";
 		playing = true;
 	}, 450);
 }
-document.getElementById("endlessButton").addEventListener("click", startGame);
+$("#endlessButton").on("click", startGame);
 
 function endGame() {
-	const menu = document.getElementById("menuWrapper");
-	const canvas = document.getElementById("canvas");
 	canvas.style.display = "block";
 	playing = false;
 	menu.classList.toggle("menuShrinkOut");
 }
 
 function pauseGame() {
-	const menu = document.getElementById("menuWrapper");
-	const canvas = document.getElementById("canvas");
 	paused = !paused;
 	menu.classList.toggle("menuShrinkOut");
 }
 
-document.addEventListener("keydown", function(e) {
-	e.preventDefault();
-	if (e.keyCode == 27 && playing) {
+$(document).on("keydown", (event) => {
+	// event.preventDefault();
+	if (event.keyCode == 27 && playing) {
 		pauseGame();
 	}
 });
